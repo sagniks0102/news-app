872f0725065ea194e7b8d964e6a96639ba44fc39
Sagnik Sarkar <49356740+sagniks0102@users.noreply.github.com>
2024-04-12 21:46:38 +0530

    Add files via upload

---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import image from '../assets/news.jpg'
+const NewsItem = ({title, description, src, url}) => {
+  return (
+    // <div className="row">
+    // <div className="col-lg-4 col-md-6 col-sm-12 col-12">
+      <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-2 px-2 py-2" style={{maxWidth:"345px"}}>
+        <img src={src?src:image} style={{height:'200px', width:'325px'}} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title"> {title.slice(0,50)}</h5>
+          <p className="card-text">
+            {description?description.slice(0,90):"News"}
+          </p>
+          <a href={url} className="btn btn-primary">
+           Read More
+          </a>
+        </div>
+      </div>
+    // </div>
+    // </div>
+  );
+};
+
+export default NewsItem;
